da08fca29f2e13493646f0d8981c7c612f47494c
yamazakiyusuke <yusuke38098@gmail.com>
2020-10-24 03:58:07 +0000

    add CRUD機能の実装

---
+++ app/controllers/properties_controller.rb
@@ -0,0 +1,63 @@
+class PropertiesController < ApplicationController
+  before_action :set_property, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @properties = Property.all
+  end
+
+  def show
+  end
+
+  def new
+    @property = Property.new
+    @property.stations.new
+    @property.stations.new
+  end
+
+  def edit
+    @property.stations.new
+  end
+
+  def create
+    @property = Property.new(property_params)
+
+    respond_to do |format|
+      if @property.save
+        format.html { redirect_to @property, notice: '新規物件を登録しました' }
+        format.json { render :show, status: :created, location: @property }
+      else
+        format.html { render :new }
+        format.json { render json: @property.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @property.update(property_params)
+        format.html { redirect_to @property, notice: '物件情報を更新しました' }
+        format.json { render :show, status: :ok, location: @property }
+      else
+        format.html { render :edit }
+        format.json { render json: @property.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @property.destroy
+    respond_to do |format|
+      format.html { redirect_to properties_url, notice: '物件情報を削除しました' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_property
+      @property = Property.find(params[:id])
+    end
+
+    def property_params
+      params.require(:property).permit(:title, :price, :adress, :old, :content, stations_attributes:[ :line, :name, :distance ] )
+    end
+end
